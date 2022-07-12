fcda9fed1db00a17113ee1ed28e9c64a68d79fd2
Kazuko Baynton <kazukobaynton@gmail.com>
2022-07-12 09:27:58 -0700

    add error message for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
+      flash[:alert] = "Login Failed"
       redirect_to '/login'
     end
   end
